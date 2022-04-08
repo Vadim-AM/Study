3ea8e16cbddd08a0f248b50dc06294372390873c
VadimM <zayaz2805@gmail.com>
2022-04-09 00:04:57 +0200

    loop for pascal triangle

--- Advanced.py
+++ Advanced.py
@@ -1,17 +1,15 @@
 def pascal(n):
     string = []
-    if n == 0:
-        string.append(1)
-        return string
-    elif n == 1:
-        string.append(1)
-        string.append(1)
-        return string
-    elif n >= 2:
-        for num in range(2, n + 2):
-            num = ((num * (num - 1)) // 2)
-            string.append(num)
-        return string
+    for i in range(n + 1):
+        temp_string = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_string.append(1)
+            else:
+                temp_string.append(string[i - 1][j - 1] + string[i-1][j])
+        string.append(temp_string)
+    return string[n]
 
 
-print(pascal(5))
+print(pascal(int(input())))
+
