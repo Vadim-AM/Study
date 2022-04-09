21ed644d2b96971e75d3aae979f443c4edf4c45a
VadimM <zayaz2805@gmail.com>
2022-04-09 16:22:31 +0200

    Return changes

--- Advanced.py
+++ Advanced.py
@@ -1,6 +1,6 @@
-def pascal(fli):
+def pascal(string_number):
     string = []
-    for i in range(fli + 1):
+    for i in range(string_number + 1):
         temp_string = []
         for j in range(i + 1):
             if j == 0 or j == i:
