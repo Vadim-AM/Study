d5f13e99aff86df2821fefff429f97caef89195f
VadimM <zayaz2805@gmail.com>
2022-04-21 00:12:52 +0200

    worked with turlte

---
+++ Turtle/red_orange_spiral.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+bgcolor('black')
+speed(0)
+hideturtle()
+for i in range(240):
+    color('red')
+    circle(i * 1.2)
+    color('orange')
+    circle(i)
+    left(3)
+    forward(3)
+done()
