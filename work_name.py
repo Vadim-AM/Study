537d797fe9eea829e3777162f2ef07cf89db60e0
VadimM <zayaz2805@gmail.com>
2022-04-21 00:12:41 +0200

    worked with turlte

--- work_name.py
+++ work_name.py
@@ -1,24 +1,12 @@
 n, m = [int(i) for i in input().split()]
-matrix = [[0] * m for _ in range(n)]
-k = 1
-one, two = 0, max(n, m) - 1
-squares = int((min(n, m) + 1) / 2)
-for i in range(squares):
-    for j in range(one, two + 1):
-        matrix[i][j] = k
-        k += 1
-    for j in range(one + 1, two):
-        matrix[j][two] = k
-        k += 1
-    for j in range(two - 1, one - 1, -1):
-        matrix[two - 1][j] = k
-        k += 1
-    for j in range(two - 1, one, -1):
-        matrix[j][one] = k
-        k += 1
-    one += 1
-    two -= 1
+result_matrix = [[0] * m for _ in range(n)]
+matrix_one = [[int(_) for _ in input().split()] for _ in range(n)]
+input()
+matrix_two = [[int(_) for _ in input().split()] for _ in range(n)]
+
 for i in range(n):
     for j in range(m):
-        print(str(matrix[i][j]).ljust(3), end='')
-    print()
+        result_matrix[i][j] = matrix_one[i][j] + matrix_two[j][i]
+
+for i in result_matrix:
+    print(*i)
